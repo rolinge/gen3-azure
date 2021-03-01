3a001e13e9d93b4b8c84aa14778f36d53ebe7055
Grondahl <kgrondah@LAMU02C8ANCMD6M.uhc.com>
2021-03-01 11:24:24 -0600

    updating dockerfile and ca-certs directory for jupyter notebooks

--- projects/gen3-kubes/Azure-infrastructure/registry.tf
+++ projects/gen3-kubes/Azure-infrastructure/registry.tf
@@ -13,3 +13,10 @@ resource "azurerm_role_assignment" "acrtofuncapp0" {
   role_definition_name = "Reader"
   principal_id         = azurerm_function_app.funcapp.identity[0].principal_id
 }
+
+resource "azurerm_role_assignment" "acrtojupyter" {
+  scope                = azurerm_container_registry.gen3.id
+  role_definition_name = "Reader"
+  principal_id         = azurerm_function_app.funcapp.identity[0].principal_id
+}
+
